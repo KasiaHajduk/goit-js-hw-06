24ddc82afc642965e728ab438ff4aaba895d0462
KasiaHajduk <kasiahajduk2014@gmail.com>
2021-11-07 23:12:24 +0100

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,18 @@
+// Napisz skrypt, który:
+
+// Policzy i wprowadzi do wiersza poleceń liczbę kategorii w ul#categories, czyli elementy li.item.
+// Dla każdego elementu li.item na liście ul#categories, znajdzie i wprowadzi do wiersza poleceń tekst nagłówka elementu (tag <h2>) i liczbę elementów w kategorii (wszystkich <li>).
+// W konsekwencji, w wierszu poleceń znajdą się takie komunikaty.
+
+const parentUl = document.querySelector("#categories");
+const categoryUl = parentUl.querySelectorAll("ul");
+console.log(`Number of categories: ${categoryUl.length}`+ "\n\n");
+const parentLi = document.querySelectorAll(".item");
+for (const parLi of parentLi) {
+    const categories = parLi.querySelector("h2");
+    console.log(`Category: ${categories.innerHTML}`);
+
+    const liNumber = parLi.querySelectorAll("li");
+    console.log(`Elements: ${liNumber.length}`+ "\n\n");
+}
+
